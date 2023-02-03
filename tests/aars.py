df06ae2fa7259e5cc0003e9be9ad01258c0ee120
Mike Hukiewitz <mike.hukiewitz@robotter.ai>
2023-02-03 18:25:30 +0100

    improve test reliability

--- tests/aars.py
+++ tests/aars.py
@@ -1,5 +1,5 @@
 import asyncio
-from typing import List
+from typing import List, Optional
 
 from aleph_client.asynchronous import get_fallback_session
 
@@ -19,6 +19,7 @@ def event_loop():
 class Book(Record):
     title: str
     author: str
+    year: Optional[int]
 
 
 class Library(Record):
@@ -32,6 +33,7 @@ async def test_store_and_index():
     new_book = await Book.create(title='Atlas Shrugged', author='Ayn Rand')
     assert new_book.title == 'Atlas Shrugged'
     assert new_book.author == 'Ayn Rand'
+    await asyncio.sleep(1)
     fetched_book = (await Book.query(title='Atlas Shrugged'))[0]
     assert new_book == fetched_book
 
@@ -39,7 +41,11 @@ async def test_store_and_index():
 @pytest.mark.asyncio
 async def test_multi_index():
     Index(Book, ['title', 'author'])
-    new_book = await Book.create(title='Lila', author='Robert M. Pirsig')
+    new_book = await Book.create(title='Lila', author='Robert M. Pirsig', year=1991)
+    # wait a few secs
+    await asyncio.sleep(1)
+    should_be_none = (await Book.query(title='Lila', author='Yo Momma'))
+    assert len(should_be_none) == 0
     fetched_book = (await Book.query(title='Lila', author='Robert M. Pirsig'))[0]
     assert new_book == fetched_book
 
@@ -55,6 +61,7 @@ async def test_amending_record():
     assert book.current_revision == 1
     assert book.revision_hashes[0] == book.id_hash
     assert book.revision_hashes[1] != book.id_hash
+    await asyncio.sleep(1)
     old_book = await book.fetch_revision(rev_no=0)
     assert old_book.title == 'Neurodancer'
     new_book = await book.fetch_revision(rev_no=1)
@@ -69,6 +76,7 @@ async def test_store_and_index_record_of_records():
         Book.create(title='The Martian', author='Andy Weir')
     )
     new_library = await Library.create(name='The Library', books=books)
+    await asyncio.sleep(1)
     fetched_library = (await Library.query(name='The Library'))[0]
     assert new_library == fetched_library
 
@@ -76,8 +84,10 @@ async def test_store_and_index_record_of_records():
 @pytest.mark.asyncio
 async def test_forget_object():
     forgettable_book = await Book.create(title="The Forgotten Book", author="Mechthild Gläser")  # I'm sorry.
+    await asyncio.sleep(1)
     await forgettable_book.forget()
     assert forgettable_book.forgotten is True
+    await asyncio.sleep(1)
     assert len(await Book.fetch(forgettable_book.id_hash)) == 0
     with pytest.raises(AlreadyForgottenError):
         await forgettable_book.forget()
@@ -90,5 +100,5 @@ async def test_store_and_wrong_query():
     assert new_book.title == 'Atlas Shrugged'
     assert new_book.author == 'Ayn Rand'
     with pytest.warns(UserWarning):
-        fetched_book = (await Book.query(title='Atlas Shrugged', index="index"))
+        fetched_book = (await Book.query(title='Atlas Shrugged', foo="bar"))
     assert len(fetched_book) == 0
